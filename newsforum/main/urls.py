e76739c7fed5a4f5bcebeeee0dee78790d59c120
aleksej <xfrbt1@gmail.com>
2022-09-15 14:02:11 +0300

    hello

---
+++ newsforum/main/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from news.views import home
+import authentication, news
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    # path('authentication/', include('authentication.urls')),
+    # path('news/', include('news.urls')),
+
+
+]
