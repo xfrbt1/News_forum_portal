b41fd9fab93b90566b2d8674584cddd7923bb8b9
aleksej <xfrbt1@gmail.com>
2022-09-22 22:27:47 +0300

    hello

--- newsforum/forum/urls.py
+++ newsforum/forum/urls.py
@@ -15,8 +15,10 @@ from forum import views
 
 urlpatterns = [
       path('', views.PostsView.as_view(), name='forum'),
-      path('post_detail/<int:pk>', views.PostDetailView.as_view(), name="post_detail"),
+      # path('post_detail/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
       path('post_create/', views.post_create, name='post_create'),
+      # path('post_detail/<int:pk>', views.AddComment.as_view(), name='add_comment'),
+      path('post_detail/<int:pk>', views.post_detail_view, name='post_detail'),
 
 ]
 
--- newsforum/forum/views.py
+++ newsforum/forum/views.py
@@ -6,51 +6,8 @@ from django.views.generic import CreateView, DetailView, ListView
 from forum.forms import PostCreateForm, CommentCreateForm
 from forum.models import Post, Comment
 
-#
-#
-# @login_required(login_url=reverse_lazy('auth_page'))
-# def post_detail(request):
-#     comment_form = CommentForm()
-#     post = get_object_or_404(Post.pk)
-#     comments = post.comments.filter()
-#     if request.method == 'POST':
-#         author = request.user.username
-#         comment_form = CommentForm(request.POST)
-#         if comment_form.is_valid():
-#             Comment.objects.create(**comment_form.cleaned_data, author=author)
-#             return redirect('forum')
-#         else:
-#             comment_form = CommentForm()
-#     return render(request, 'forum:post', context={'comment_form': comment_form,
-#             "comments": comments},)
-
-#
-#
-#
-# @login_required(login_url=reverse_lazy('auth_page'))
-# def forum(request):
-#     posts = Post.objects.order_by('-publish_time')
-#     return render(request, 'forum/forum_page.html', {'posts': posts})
-#
-#
-#
-#
-# @login_required(login_url=reverse_lazy('auth_page'))
-# def post_create(request):
-#     if request.method == 'POST':
-#         author = request.user.username
-#         form = PostCreateForm(request.POST)
-#         if form.is_valid():
-#             Post.objects.create(**form.cleaned_data, author=author)
-#             return redirect('forum')
-#     else:
-#         form = PostCreateForm()
-#     return render(request, 'forum/post_create.html', {'form': form})
-
 
 class PostsView(View):
-    # model = Post
-    # queryset = Post.objects.filter()
 
     def get(self, request):
         posts = Post.objects.order_by('-publish_time')
@@ -68,7 +25,18 @@ class PostDetailView(View):
 
 
 
-
+@login_required(login_url=reverse_lazy('auth_page'))
+def post_detail_view(request, pk):
+    post = Post.objects.get(id = pk)
+    post_comments = Comment.objects.filter(post_id=post)
+    if request.method == "POST":
+        comment_form = CommentCreateForm(request.POST)
+        if comment_form.is_valid():
+            Comment.objects.create(**comment_form.cleaned_data, author=request.user.username, post_id=post.pk)
+            return redirect('forum')
+    else:
+        comment_form = CommentCreateForm()
+    return render(request, 'forum/post_detail.html', {'post': post, 'post_comments': post_comments, 'comment_form': comment_form})
 
 
 
@@ -86,14 +54,3 @@ def post_create(request):
 
 
 
-
-@login_required(login_url=reverse_lazy('auth_page'))
-def comment_add(request):
-    if request.method == "POST":
-        comment_form = CommentCreateForm(request.POST)
-        if comment_form.is_valid():
-            Comment.objects.create(**comment_form.cleaned_data, author=request.user.username, post=Post.pk)
-            return redirect('forum')
-    else:
-        comment_form = CommentCreateForm()
-    return render(request, 'forum/post_detail.html', {'comment_form': comment_form})
