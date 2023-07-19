1cce4cb4085b3a3e30ef5593986a13f16e5dc405
Alexey Kutanov <106831446+xfrbt1@users.noreply.github.com>
2023-07-19 23:20:21 +0300

    Update urls.py

--- newsforum/forum/urls.py
+++ newsforum/forum/urls.py
@@ -3,16 +3,6 @@ from django.urls import path, include
 # from forum.views import post_detail
 from forum import views
 
-# urlpatterns = [
-#     path('', views.forum, name='forum'),
-#     path('post_create/', views.post_create, name='post_create'),
-#     path('post_detail/<int:pk>', views.post_detail, name='post_detail'),
-#
-#
-#
-#
-# ]
-
 urlpatterns = [
       path('', views.PostsView.as_view(), name='forum'),
       # path('post_detail/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
@@ -21,17 +11,5 @@ urlpatterns = [
       path('post_detail/<int:pk>', views.post_detail_view, name='post_detail'),
 
 ]
-
-
-
-# <div class="outer">
-# <a href="{% url 'post_create' %}">
-#         <button>
-#             WRITE POST
-#         </button>
-# </a>
-# </div>
-#
-#
 
 
